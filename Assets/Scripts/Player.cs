67b1b6e3da169f85ba8f9bd3be2d0da6dfb4ee60
EluardJ <ju.eluard@laposte.net>
2019-10-09 19:47:18 +0200

    orbiting + move modes
    first version with a functionning switch between orbiting and move mode

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,9 +17,10 @@ public class Player : MonoBehaviour
     private Vector2 moveDirection;
     private GameObject nearPlanet;
     private float lastDistanceToPlanet;
+    private float distanceToOrbitingPlanet;
     private Vector3 orbitOrigin;
-    private Vector3 orbitAxis = new Vector3(0, 0, 1);
     private float buttonTimer;
+    private float currentSpeed = 7;
     [SerializeField] private float maxTimer = 2;
     [SerializeField] private float movementSpeedModifier = 3;
     #endregion
@@ -29,36 +30,31 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb2D = gameObject.GetComponent<Rigidbody2D>();
-        nearPlanet = GameObject.Find("OrbitPlanet");
+        nearPlanet = GameObject.Find("StartPlanet");
         orbitOrigin = nearPlanet.transform.position;
+        distanceToOrbitingPlanet = Vector2.Distance(orbitOrigin, transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (mode == "orbital")
-        {
-            OrbitAround();
-        }
-
-        RotateToMovingDirection();
+        Debug.Log(rb2D.velocity.magnitude);
 
         UsingSpaceKey();
 
-        if(mode == "in gravity field")
+        if (mode == "in gravity field")
+        // determine if the ship is going near or away the current planet
         {
             float distanceToPlanet = Vector2.Distance(transform.position, orbitOrigin);
-            Debug.Log("distance : " + distanceToPlanet + "   last dist : " + lastDistanceToPlanet);
 
-            if(distanceToPlanet <= lastDistanceToPlanet)
+            if (distanceToPlanet > lastDistanceToPlanet)
             {
-                Debug.Log("approaching");
-            }
-            else
-            {
-                Debug.Log("going away");
+                //enter orbital mode if the conditions are met
+                currentSpeed = rb2D.velocity.magnitude;
                 rb2D.velocity = Vector2.zero;
                 nearPlanet.GetComponent<PointEffector2D>().enabled = false;
+                distanceToOrbitingPlanet = Vector2.Distance(orbitOrigin, transform.position);
+
                 mode = "orbital";
             }
 
@@ -67,14 +63,38 @@ public class Player : MonoBehaviour
 
     }
 
+    private void FixedUpdate()
+    {
+        if (mode == "orbital")
+        {
+            OrbitAround();
+        }
+
+        RotateToMovingDirection();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         mode = "in gravity field";
         nearPlanet = other.gameObject;
         orbitOrigin = nearPlanet.transform.position;
         lastDistanceToPlanet = Vector2.Distance(transform.position, orbitOrigin);
-        Debug.Log(mode);
+
+        if(Vector2.SignedAngle(rb2D.velocity, (transform.position - orbitOrigin)) > 0)
+        {
+            movementSpeedModifier = -Mathf.Abs(movementSpeedModifier);
+        }
+        else
+        {
+            movementSpeedModifier = Mathf.Abs(movementSpeedModifier);
+        }
     }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        other.GetComponent<PointEffector2D>().enabled = true;
+    }
+
     #endregion
 
     #region Functions
@@ -91,16 +111,25 @@ public class Player : MonoBehaviour
     }
 
     private void OrbitAround()
-    // makes the player orbit around an OrbitpLanet
+    // makes the player orbit around a Planet
     {
-            transform.RotateAround(orbitOrigin, orbitAxis, Time.deltaTime * movementSpeedModifier * 10);
+        Vector3 dir = (orbitOrigin - transform.position);
+        Vector3 cross = Vector3.Cross(dir, Vector3.forward).normalized;
+        rb2D.velocity = cross * movementSpeedModifier;
+
+        //to correct the imprecision of the orbiting code
+        float diff = Vector2.Distance(orbitOrigin, transform.position) - distanceToOrbitingPlanet;
+        transform.position += dir.normalized * diff;
+
+        //Debug.Log("distance : " + Vector2.Distance(orbitOrigin, transform.position));
     }
 
     private void UsingSpaceKey()
     // dictate what the space key is doing according to the situation
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && mode == "orbital")
         {
+            //charges the timer
             buttonTimer += Time.deltaTime;
         }
 
@@ -108,8 +137,9 @@ public class Player : MonoBehaviour
         {
             if (mode == "orbital")
             {
+                //launches the ship, going into moving mode
                 mode = "moving";
-                rb2D.AddForce(transform.right * Mathf.Clamp(buttonTimer, 0, maxTimer) * movementSpeedModifier, ForceMode2D.Impulse);
+                rb2D.AddForce(transform.right * Mathf.Clamp(buttonTimer, 0, maxTimer) * Mathf.Abs(movementSpeedModifier), ForceMode2D.Impulse);
                 Debug.Log(Mathf.Clamp(buttonTimer, 0, maxTimer));
             }
 

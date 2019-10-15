99ee16463055486758ef5fae9057118c2daf4e81
EluardJ <ju.eluard@laposte.net>
2019-10-15 19:30:24 +0200

    changed the gravity field script
    changed the gravity field script to planet.cs and puted it directly to the parent in the prefab and not on the child

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -8,30 +8,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GravityField : MonoBehaviour
+public class Planet : MonoBehaviour
 {
     #region Variables
     private float rotationSpeed = 0;
     private Vector3 rotationVector;
+    private GameObject GravityField;
     #endregion
 
     #region Unity's Functions
     // Start is called before the first frame update
     void Start()
     {
+        GravityField = transform.GetChild(1).gameObject;
+
         while(rotationSpeed == 0)
         {
             rotationSpeed = Random.Range(-50, 50);
         }
         rotationVector = new Vector3(0, 0, rotationSpeed/100);
 
-        gameObject.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        GravityField.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotationVector);
+        GravityField.transform.Rotate(rotationVector);
     }
     #endregion
 
